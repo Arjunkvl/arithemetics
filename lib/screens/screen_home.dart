714424bc561c293d59cb79a6cecbe6113bc15623
Arjunkvl <arjunaskvl@gmail.com>
2023-05-07 18:46:13 +0530

    home screen ui completed with text field update

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,3 +1,4 @@
+import 'package:arithemetics/screens/text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -41,95 +42,14 @@ class _HomePageState extends State<HomePage> {
                   Padding(
                     padding: EdgeInsets.all(
                         MediaQuery.of(context).size.width * 0.05),
-                    child: Center(
+                    child: const Center(
                       child: Row(
                         mainAxisSize: MainAxisSize.max,
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.26,
-                            child: TextField(
-                              textAlign: TextAlign.center,
-                              maxLength: 1,
-                              inputFormatters: [
-                                LengthLimitingTextInputFormatter(1),
-                                FilteringTextInputFormatter.digitsOnly,
-                              ],
-                              showCursor: false,
-                              style: const TextStyle(
-                                  fontSize: 50,
-                                  fontFamily: 'mono',
-                                  fontWeight: FontWeight.w900,
-                                  color: Color(0xff5B4F47)),
-                              decoration: const InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                          width: 5, color: Color(0xffffffff)),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(15))),
-                                  enabledBorder: OutlineInputBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(15)),
-                                      borderSide: BorderSide(
-                                          width: 5, color: Color(0xff745E4D)))),
-                            ),
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.26,
-                            child: TextField(
-                              textAlign: TextAlign.center,
-                              maxLength: 1,
-                              inputFormatters: [
-                                LengthLimitingTextInputFormatter(1),
-                                FilteringTextInputFormatter.digitsOnly,
-                              ],
-                              showCursor: false,
-                              style: const TextStyle(
-                                  fontSize: 50,
-                                  fontFamily: 'mono',
-                                  fontWeight: FontWeight.w900,
-                                  color: Color(0xff5B4F47)),
-                              decoration: const InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                          width: 5, color: Color(0xffffffff)),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(15))),
-                                  enabledBorder: OutlineInputBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(15)),
-                                      borderSide: BorderSide(
-                                          width: 5, color: Color(0xff745E4D)))),
-                            ),
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.26,
-                            child: TextField(
-                              textAlign: TextAlign.center,
-                              maxLength: 1,
-                              inputFormatters: [
-                                LengthLimitingTextInputFormatter(1),
-                                FilteringTextInputFormatter.digitsOnly,
-                              ],
-                              showCursor: false,
-                              style: const TextStyle(
-                                  fontSize: 50,
-                                  fontFamily: 'mono',
-                                  fontWeight: FontWeight.w900,
-                                  color: Color(0xff5B4F47)),
-                              decoration: const InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                          width: 5, color: Color(0xffffffff)),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(15))),
-                                  enabledBorder: OutlineInputBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(15)),
-                                      borderSide: BorderSide(
-                                          width: 5, color: Color(0xff745E4D)))),
-                            ),
-                          ),
+                          BoxtextField(),
+                          BoxtextField(),
+                          BoxtextField(),
                         ],
                       ),
                     ),
--- lib/screens/text_field.dart
+++ lib/screens/text_field.dart
@@ -6,31 +6,28 @@ class BoxtextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.26,
-        child: TextField(
-          textAlign: TextAlign.center,
-          maxLength: 1,
-          inputFormatters: [
-            LengthLimitingTextInputFormatter(1),
-            FilteringTextInputFormatter.digitsOnly,
-          ],
-          showCursor: false,
-          style: const TextStyle(
-              fontSize: 50,
-              fontFamily: 'mono',
-              fontWeight: FontWeight.w900,
-              color: Color(0xff5B4F47)),
-          decoration: const InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(width: 5, color: Color(0xffffffff)),
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  borderSide: BorderSide(width: 5, color: Color(0xff745E4D)))),
-        ),
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.26,
+      child: TextField(
+        textAlign: TextAlign.center,
+        maxLength: 1,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(1),
+          FilteringTextInputFormatter.digitsOnly,
+        ],
+        showCursor: false,
+        style: const TextStyle(
+            fontSize: 50,
+            fontFamily: 'mono',
+            fontWeight: FontWeight.w900,
+            color: Color(0xff5B4F47)),
+        decoration: const InputDecoration(
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 5, color: Color(0xffffffff)),
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                borderSide: BorderSide(width: 5, color: Color(0xff745E4D)))),
       ),
     );
   }
