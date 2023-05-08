b5547012de69552de766386d9a205a030a918c8a
Arjunkvl <arjunaskvl@gmail.com>
2023-05-08 09:23:51 +0530

    added a custom text widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,25 +5,60 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                'Common Difference',
-                style: TextStyle(fontFamily: 'mono'),
-              ),
-              Text(
-                '2',
-                style: TextStyle(fontFamily: 'mono'),
-              )
-            ],
+    return Scaffold(
+      body: FocusScope(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.height * 0.05,
+                top: MediaQuery.of(context).size.height * 0.15,
+                right: MediaQuery.of(context).size.height * 0.05),
+            child: const Column(
+              children: [
+                CustomText(label: 'Common Difference', eq: '2'),
+                CustomText(label: 'sum equation', eq: '2'),
+                CustomText(label: 'term equation', eq: '2'),
+              ],
+            ),
           ),
-          Row(),
-          Row(),
-        ],
+        ),
       ),
+    );
+  }
+}
+
+class CustomText extends StatelessWidget {
+  final String label;
+  final String eq;
+  const CustomText({super.key, required this.label, required this.eq});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.05),
+            child: Text(
+              label,
+              style: const TextStyle(
+                fontFamily: 'mono',
+                color: Color(0xff5B4F47),
+                fontSize: 40,
+              ),
+            ),
+          ),
+        ),
+        Text(
+          eq,
+          style: const TextStyle(
+            fontFamily: 'mono',
+            color: Color(0xff5B4F47),
+            fontSize: 40,
+          ),
+        )
+      ],
     );
   }
 }
