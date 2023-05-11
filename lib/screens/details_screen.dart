29041773462f1fb5607793aac459715175fe03b4
Arjunkvl <arjunaskvl@gmail.com>
2023-05-11 17:45:02 +0530

    added sum function

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -13,6 +13,8 @@ class DetailsScreen extends StatefulWidget {
 
 class _DetailsScreenState extends State<DetailsScreen> {
   TextEditingController term = TextEditingController();
+  TextEditingController term2 = TextEditingController();
+
   String text = '';
 
   @override
@@ -43,7 +45,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                 ),
                 Button(
                     label: 'Calculate Sum',
-                    action: () => findTerm(context, term)),
+                    action: () => findSum(context, term)),
                 Button(
                   label: 'Find Terms',
                   action: () => findTerm(context, term),
@@ -153,6 +155,104 @@ class _DetailsScreenState extends State<DetailsScreen> {
       },
     );
   }
+
+  void findSum(context, term) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Column(
+              children: [
+                const Text(
+                  'Enter the Number',
+                  style: TextStyle(
+                    fontFamily: 'mono',
+                    color: Color(0xffffffff),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.18,
+                      right: MediaQuery.of(context).size.width * 0.18,
+                      top: MediaQuery.of(context).size.width * 0.05),
+                  child: TextField(
+                    showCursor: false,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontFamily: 'mono',
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff5B4F47),
+                    ),
+                    onSubmitted: (value) {
+                      setState(() {
+                        text =
+                            'The sum of first ${term2.text} terms is ${sum(widget.controller, term2)} ';
+                      });
+                    },
+                    controller: term2,
+                    decoration: const InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 5, color: Color(0xffffffff)),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 5, color: Color(0xffffffff)),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10)))),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.width * 0.1),
+                Text(
+                  text,
+                  style: const TextStyle(
+                      fontFamily: 'mono',
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff5B4F47)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Close',
+                            style: TextStyle(
+                                fontFamily: 'mono',
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff5B4F47)))),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            text = '';
+                            term2.text = '';
+                          });
+                        },
+                        child: const Text(
+                          'Clear',
+                          style: TextStyle(
+                              fontFamily: 'mono',
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff5B4F47)),
+                        )),
+                  ],
+                )
+              ],
+            ),
+          ),
+          backgroundColor: Color(0xffBD9479),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          )),
+        );
+      },
+    );
+  }
 }
 
 String commonDifference(controller) {
@@ -189,6 +289,17 @@ String thTerm(controller, term) {
   ContextModel cm = ContextModel();
   double d = exp.evaluate(EvaluationType.REAL, cm);
   Expression t = p.parse('$d * ${term.text}+${controller[0].text}-$d');
+  double r = t.evaluate(EvaluationType.REAL, cm);
+  return r.toString();
+}
+
+String sum(controller, term) {
+  Parser p = Parser();
+  Expression exp = p.parse('${controller[1].text}-${controller[0].text}');
+  ContextModel cm = ContextModel();
+  double d = exp.evaluate(EvaluationType.REAL, cm);
+  Expression t =
+      p.parse('${term.text}/2*(2*${controller[0].text} + (${term.text}-1)*$d)');
   double r = t.evaluate(EvaluationType.REAL, cm);
   return r.toString();
 }
